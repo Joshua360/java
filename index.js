6c1e13bbb79c0d8812d77c7144603a87fa12fb01
Joshua360 <joshuaowuor27@gmail.com>
2021-08-04 17:12:40 +0300

    2

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-//packages I need: express, nodemon, cookie-server, body-server,pug
+
 
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+ 
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 app.set("view engine", "pug");
 
 app.get("/", (req,res)=>{
-    res.render("index");
+    res.render("index", {name:req.cookies.name});
 });
 
 app.get("/hello", (req,res)=>{
     res.render("hello");
+    console.log(req.body)
+
+   
 });
 
 
-app.post("/hello", (req,res)=>{
-    console.log(req.body)
-    res.redirect("/");
+
+
+app.post('/hello', (req,res)=>{
+    console.dir(req.body)
+    res.cookie('name', req.body.name);
+    res.redirect('/');
 });
 
 const port = process.env.PORT || 3000;
